aec4d493907176105f58419164cb3bfe1dbe0d89
Jazer Barclay <contact@jazer.co.uk>
2020-07-28 22:24:04 +0100

    Updated for installation

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Create ssh folder and key
+mkdir -p /home/`whoami`/.ssh
+pushd /home/`whoami`/.ssh
+ssh-keygen -o -a 100 -t ed25519 -f /home/`whoami`/.ssh/id_ed25519
+popd
+
+# Add wheel and tty_tickets to sudoers file
+echo $'# One sudo login authorises all other terminals a free upgrade\nDefaults !tty_tickets\n\n# Uncomment below to allow sudo without password on wheel users\n%wheel ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
+
+# Setup dotfiles from github
 pushd /home/`whoami`
 git clone --bare https://github.com/jazerbarclay/dotfiles.git /home/`whoami`/dotfiles
 git --git-dir=/home/`whoami`/dotfiles --work-tree=/home/`whoami` checkout -f
